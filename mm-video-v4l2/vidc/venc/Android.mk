317a4023b9401468f7e4710b6c96d17aa8716fb1
Alex Naidis <alex.naidis@linux.com>
2016-07-27 23:04:36 +0200

    mm-video: vidc: venc: enable VQZIP if vendor/head is present
    * vendor/head contains proprietary definition which we may not distribute
    * With this we can ship VQZIP support in official builds
    
    Change-Id: I3e21f079e5a38f989e0f0088b3915adde70afaa1
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -54,7 +54,9 @@ libmm-venc-def += -D_NV21_
 endif
 
 ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_VQZIP)),true)
+ifeq ($(VENDOR_HEAD_IS_PRESENT),true)
 libmm-venc-def += -D_VQZIP_
+endif
 endif
 
 ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_PQ)),true)
