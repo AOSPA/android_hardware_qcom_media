187cfc638103c7d4d64bb91396d46ee50153b464
Evan Anderson <evananderson@aospa.co>
2016-11-06 17:20:23 -0500

    hals: Restructure for caf
    * msm8994 only
    
    Change-Id: I79c68187e7ccd88f5c79ea21679353e84d399689
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- Android.mk
+++ Android.mk
@@ -1,27 +1,14 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-  # for msm8996 targets, use the msm8996 directory, for all other targets
-  # use the msm8974 directory.
-  ifneq ($(filter msm8996, $(TARGET_BOARD_PLATFORM)),)
-    QCOM_MEDIA_ROOT := $(call my-dir)/msm8996
-  else
-    QCOM_MEDIA_ROOT := $(call my-dir)/msm8974
-  endif
+QCOM_MEDIA_ROOT := $(call my-dir)
+$(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996,$(TARGET_BOARD_PLATFORM)),)
-    include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-    include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-  endif
+#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
+ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+endif
 
-  ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
-    include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
-  endif
+ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
+include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
-  ifneq ($(filter msm8610 msm8226 msm8084 msm8952 msm8992 msm8994 msm8996,$(TARGET_BOARD_PLATFORM)),)
-    include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-  endif
-
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996,$(TARGET_BOARD_PLATFORM)),)
-    include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
-  endif
 endif
--- msm8974/mm-video-v4l2/vidc/Android.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(LOCAL_PATH)/vdec.mk $(LOCAL_PATH)/venc.mk
