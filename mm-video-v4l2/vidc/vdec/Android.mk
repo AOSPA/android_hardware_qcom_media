14240ce9ef2e20f02544d8a6b2f41bf8b56d5f63
Divya Sharma <divyash@codeaurora.org>
2017-03-30 00:52:43 -0700

    Media : Ignore -Wswitch
    Change-Id: Ifcb5cc2b925ec4f5ac0226bc27067d2b80f98113

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -99,7 +99,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
-LOCAL_CFLAGS                    := $(libmm-vdec-def) -Werror
+LOCAL_CFLAGS                    := $(libmm-vdec-def) -Werror -Wno-switch
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
 
