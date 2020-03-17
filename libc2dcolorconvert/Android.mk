d0246ae7417d789a39afdf4d1632df8e2c0437ff
Suprith Malligere Shankaregowda <supgow@codeaurora.org>
2020-03-17 20:42:10 +0530

    libc2dcolorconvert: Create system version of the library
    Create a system version of the library to be used by
    an internal recording test app.
    
    Change-Id: I41e8a15a091db9b7435e226fbcad23eb9bd91074

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
+#======================================================================
+# libc2dcolorconvert - vendor library
+#======================================================================
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -27,3 +30,36 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
+
+ifeq ($(TARGET_BOARD_AUTO),true)
+#======================================================================
+# libc2dcolorconvert - system library - cannot build if QSSI is enabled
+#======================================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        C2DColorConverter.cpp
+
+LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/adreno
+
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_HEADER_LIBRARIES := \
+        libutils_headers \
+        libhardware_headers \
+        display_intf_headers
+
+LOCAL_SHARED_LIBRARIES := liblog libdl
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := libc2dcolorconvert_system
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+
