bcb722c529e21daf2baa6dda452c463b41534545
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-03-27 22:29:47 +0530

    configs: msmnile: guard c2_manifest if media-legacy component is being used
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I6dd78fb6a9d843ced93be76717c8e87022808672

--- conf_files/msmnile/msmnile.mk
+++ conf_files/msmnile/msmnile.mk
@@ -22,7 +22,9 @@ PRODUCT_PACKAGES += \
 # Enable Codec2.0 HAL for pure AOSP variants.
 ifeq ($(GENERIC_ODM_IMAGE),true)
     $(warning "Forcing codec2.0 HW for generic odm build variant")
+    ifeq (,$(filter media-legacy, $(TARGET_COMMON_QTI_COMPONENTS)))
     DEVICE_MANIFEST_FILE += hardware/qcom/media/conf_files/msmnile/c2_manifest_vendor.xml
+    endif
     #Set default ranks and rank Codec 2.0 over OMX codecs
     PRODUCT_ODM_PROPERTIES += debug.stagefright.ccodec=4
     PRODUCT_ODM_PROPERTIES += debug.stagefright.omx_default_rank=1000
@@ -32,7 +34,9 @@ ifeq ($(GENERIC_ODM_IMAGE),true)
 
 else ifneq ($(TARGET_FWK_SUPPORTS_AV_VALUEADDS),false)
     $(warning "Enabling codec2.0 non-audio SW only for non-generic odm build variant")
+    ifeq (,$(filter media-legacy, $(TARGET_COMMON_QTI_COMPONENTS)))
     DEVICE_MANIFEST_FILE += hardware/qcom/media/conf_files/msmnile/c2_manifest.xml
+    endif
     PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=0
     PRODUCT_COPY_FILES += \
       device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
@@ -40,7 +44,9 @@ else ifneq ($(TARGET_FWK_SUPPORTS_AV_VALUEADDS),false)
 
 else
     $(warning "Compiling without value-added features")
+    ifeq (,$(filter media-legacy, $(TARGET_COMMON_QTI_COMPONENTS)))
     DEVICE_MANIFEST_FILE += hardware/qcom/media/conf_files/msmnile/c2_manifest.xml
+    endif
     PRODUCT_COPY_FILES += \
       device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
       device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
