cbcf3318f4768741e436af582fb726ac0c067a7e
Yuan Si <do4suki@gmail.com>
2023-05-17 09:18:49 +0800

    mm-video-v4l2: venc: Include fastcv headers lib
    Change-Id: I44dd154c25df604d151d1ed7466e3d9e4db9d5eb
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -102,7 +102,8 @@ LOCAL_HEADER_LIBRARIES := \
         libcutils_headers \
         libutils_headers \
         libhardware_headers \
-        display_headers
+        display_headers \
+        fastcv_headers
 
 LOCAL_C_INCLUDES                := $(libmm-venc-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-venc-add-dep)
