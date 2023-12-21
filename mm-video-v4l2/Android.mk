ac0e43aa32c508f5bb222a41bc0738b3ad79c4eb
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-12-21 18:43:50 +0000

    media: Define module to fastcv_headers
    Change-Id: I5227f9545cd459c8b686e16b249b8ab6bc5a430b
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- mm-video-v4l2/Android.mk
+++ mm-video-v4l2/Android.mk
@@ -1,2 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fastcv_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/vidc/venc/inc
+include $(BUILD_HEADER_LIBRARY)
