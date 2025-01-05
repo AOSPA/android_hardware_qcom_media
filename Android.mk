e6ea2f631adae4c3c9f51249ec328f236b3439fd
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 12:17:22 +0200

    mm-core: Convert to blueprint
    Change-Id: I725cd7698463691dadfcb0f7aa3bfd92d127e5ed
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- Android.mk
+++
@@ -1,8 +0,0 @@
-QCOM_MEDIA_ROOT := $(call my-dir)
-
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
-include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-endif
-
-endif
