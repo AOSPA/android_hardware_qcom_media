997c8fb989a7985fffeaa78dabb682d9df6f7141
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 05:56:22 +0200

    mm-video-v4l2: Remove SW OMX codecs, unused files & flags
    Change-Id: I0f73d7f82a8536a2913a2be3c26f00ef2393fd2b
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -24,18 +24,6 @@ libmm-vdec-def += -Wno-parentheses
 libmm-vdec-def += -D_ANDROID_ICS_
 libmm-vdec-def += -DPROCESS_EXTRADATA_IN_OUTPUT_PORT
 
-TARGETS_THAT_HAVE_VENUS_HEVC := apq8084 msm8994 msm8996
-# KONA_TODO_UPDATE: Disable SW codec for Kona for now
-TARGETS_THAT_DONT_NEED_SW_VDEC := msm8226 msm8916 msm8992 msm8996 sdm660 msm8998 msm8909
-
-ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_HAVE_VENUS_HEVC)),true)
-libmm-vdec-def += -DVENUS_HEVC
-endif
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm8610)
-libmm-vdec-def += -DSMOOTH_STREAMING_DISABLED
-endif
-
 libmm-vdec-def += -D_UBWC_
 
 ifeq ($(TARGET_BOARD_PLATFORM),bengal)
@@ -46,20 +34,6 @@ ifeq ($(TARGET_USES_ION),true)
 libmm-vdec-def += -DUSE_ION
 endif
 
-ifneq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) >= 18 ))" )))
-libmm-vdec-def += -DANDROID_JELLYBEAN_MR1=1
-endif
-
-ifeq ($(call is-board-platform-in-list, $(MASTER_SIDE_CP_TARGET_LIST)),true)
-libmm-vdec-def += -DMASTER_SIDE_CP
-endif
-
-ifeq ($(call is-platform-sdk-version-at-least,27),true) # O-MR1
-libmm-vdec-def += -D_ANDROID_O_MR1_DIVX_CHANGES
-endif
-
-include $(CLEAR_VARS)
-
 # Common Includes
 libmm-vdec-inc          := $(LOCAL_PATH)/inc
 libmm-vdec-inc          += $(LIBION_HEADER_PATHS)
@@ -68,15 +42,7 @@ libmm-vdec-inc          += $(TOP)/hardware/qcom/media/mm-core/inc
 libmm-vdec-inc          += hardware/qcom/media/libplatformconfig
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/adreno
 libmm-vdec-inc      	+= $(TOP)/hardware/qcom/media/libc2dcolorconvert
-libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/SwVdec
-libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/swvdec
 libmm-vdec-inc      	+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-
-
-ifeq ($(PLATFORM_SDK_VERSION), 18)  #JB_MR2
-libmm-vdec-def += -DANDROID_JELLYBEAN_MR2=1
-libmm-vdec-inc += $(TOP)/hardware/qcom/media/libstagefrighthw
-endif
 
 # Common Dependencies
 libmm-vdec-add-dep := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
@@ -134,52 +100,6 @@ LOCAL_SRC_FILES         += src/omx_vdec_v4l2.cpp
 LOCAL_SRC_FILES         += src/omx_vdec_v4l2_params.cpp
 
 include $(BUILD_SHARED_LIBRARY)
-
-
-
-
-# ---------------------------------------------------------------------------------
-# 			Make the Shared library (libOmxSwVdec)
-# ---------------------------------------------------------------------------------
-
-include $(CLEAR_VARS)
-ifneq "$(wildcard $(QCPATH) )" ""
-ifneq ($(call is-board-platform-in-list, $(TARGETS_THAT_DONT_NEED_SW_VDEC)),true)
-
-LOCAL_MODULE                  := libOmxSwVdec
-LOCAL_MODULE_TAGS             := optional
-LOCAL_VENDOR_MODULE           := true
-LOCAL_CFLAGS                  := $(libmm-vdec-def)
-
-ifeq ($(TARGET_ENABLE_VIDC_INTSAN), true)
-LOCAL_SANITIZE := integer_overflow
-ifeq ($(TARGET_ENABLE_VIDC_INTSAN_DIAG), true)
-$(warning INTSAN_DIAG_ENABLED)
-LOCAL_SANITIZE_DIAG := integer_overflow
-endif
-endif
-
-LOCAL_HEADER_LIBRARIES := \
-        media_plugin_headers \
-        libnativebase_headers \
-        libutils_headers \
-        libhardware_headers \
-        display_headers
-
-LOCAL_C_INCLUDES              += $(libmm-vdec-inc)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(libmm-vdec-add-dep)
-
-LOCAL_PRELINK_MODULE          := false
-LOCAL_STATIC_LIBRARIES        := libOmxVidcCommon
-LOCAL_SHARED_LIBRARIES        := liblog libcutils libc2dcolorconvert libion
-LOCAL_SHARED_LIBRARIES        += libswvdec
-
-LOCAL_SRC_FILES               := src/omx_swvdec.cpp
-LOCAL_SRC_FILES               += src/omx_swvdec_utils.cpp
-
-include $(BUILD_SHARED_LIBRARY)
-endif
-endif
 
 # ---------------------------------------------------------------------------------
 #                END
--- product.mk
+++ product.mk
@@ -14,8 +14,6 @@ ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)), true)
 
 MM_VIDEO := ExoplayerDemo
 MM_VIDEO += libc2dcolorconvert
-MM_VIDEO += libOmxSwVdec
-MM_VIDEO += libOmxSwVencMpeg4
 MM_VIDEO += libOmxVdec
 MM_VIDEO += libOmxVenc
 MM_VIDEO += libstagefrighthw
