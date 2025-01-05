27d9be3a827ba62d208eed6db4c72c0b664169a4
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 04:12:09 +0200

    libc2dcolorconvert: Cleanup dependencies & convert to blueprint
    Change-Id: I92c42e7eef8eb1c9644ae1e8e043195406a9cb07

--- Android.mk
+++ Android.mk
@@ -16,6 +16,5 @@ endif
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 ifeq ($(VIDC_STUB_HAL),false)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif #VIDC_STUB_HAL
 endif
