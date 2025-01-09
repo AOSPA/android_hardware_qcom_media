6651da1593e41501fe4e7703965a6b7115c89200
Michael Bestas <mkbestas@lineageos.org>
2025-01-09 09:41:17 +0200

    libarbitrarybytes: Convert to blueprint
    Change-Id: I26f94707500ff244787b8c64118d928f0abd2fde
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- Android.mk
+++
@@ -1,8 +0,0 @@
-QCOM_MEDIA_ROOT := $(call my-dir)
-
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
-include $(QCOM_MEDIA_ROOT)/libarbitrarybytes/Android.mk
-ifeq ($(ENABLE_HYP),true)
-include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
-endif
-endif
